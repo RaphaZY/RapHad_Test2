c977d74750cca77a678bd1f73a34589cffba12b3
Hadson00 <hadson798@gmail.com>
2024-04-17 21:21:19 -0300

    c

--- calculadora.py
+++ calculadora.py
@@ -4,48 +4,63 @@ class CalculadoraCientifica:
     def __init__(self):
         self.ultimo_resultado = None
     
+    def filtrar(self, n1, n2 = 0):
+        if type(n1) == str or type(n2) == str:
+            raise TypeError('Idiota, é número, não letra')
+
     def adicao(self, n1:int, n2:int):
+        self.filtrar(n1,n2)
         self.ultimo_resultado = n1 + n2
         return self.ultimo_resultado
-    
+
     def subtracao(self, n1:int, n2:int):
+        self.filtrar(n1,n2)
         self.ultimo_resultado = n1 - n2
         return self.ultimo_resultado
     
     def multiplicacao(self, n1:int, n2:int):
+        self.filtrar(n1,n2)
         self.ultimo_resultado = n1 * n2
         return self.ultimo_resultado
     
     def divisao(self, n1:int, n2:int):
-        if n2 == 0 or n1 == 0:
-            raise ZeroDivisionError('Divisão Por Zero')
+        self.filtrar(n1,n2)
         self.ultimo_resultado = n1 / n2
         return self.ultimo_resultado
     
     def potenciacao(self, base:int, expoente:int):
+        self.filtrar(base, expoente)
         self.ultimo_resultado = base ** expoente
         return self.ultimo_resultado
 
     def raiz_quadrada(self, n1:int):
+        self.filtrar(n1)
         if n1 < 0:
            raise ValueError('Número negativo inserido')
         self.ultimo_resultado = math.sqrt(n1)
         return self.ultimo_resultado
     
     def logaritmo(self, log, base):
+        self.filtrar(log,base)
         return math.log(log,base)
     
     def seno(self,angulo):
+        self.filtrar(angulo)
         radianos = math.radians(angulo)
         self.ultimo_resultado = f"{math.sin(radianos):.2f}"
         return self.ultimo_resultado
     
     def cosseno(self,angulo):
+        self.filtrar(angulo)
         radianos = math.radians(angulo)
         self.ultimo_resultado =f"{math.cos(radianos):.2f}"
         return self.ultimo_resultado
 
     def tangente(self,angulo):
+        self.filtrar(angulo)
         radianos = math.radians(angulo)
         self.ultimo_resultado = f"{math.tan(radianos):.2f}"
         return self.ultimo_resultado
+    
+#c = CalculadoraCientifica()
+#print()
--- teste-uni.py
+++ teste-uni.py
@@ -6,18 +6,29 @@ class TesteCalculadora(unittest.TestCase):
     def setUp(self):
         self.calc = CalculadoraCientifica()
 
+    def test_filtrar(self):
+        pass
+
     def test_adicao(self):
         self.assertEqual(self.calc.adicao(150,150),300)
         self.assertEqual(self.calc.adicao(123,432),555)
         self.assertNotEqual(self.calc.adicao(4,2),7)
         self.assertNotEqual(self.calc.adicao(20,10),31)
+        self.assertEqual(self.calc.adicao(-1,-1),-2)
         self.assertIsNotNone(self.calc.adicao(20,30))
-    
+        #with self.assertRises(TypeError):
+            #self.calc.adicao('a','b')
+
     def test_subtracao(self):
         self.assertEqual(self.calc.subtracao(2,2),0)
         self.assertEqual(self.calc.subtracao(10,5),5)
         self.assertEqual(self.calc.subtracao(11,12),-1)
-        self.assertEqual
-        
+        self.assertNotEqual(self.calc.subtracao(50,40),11)
+        self.assertIsNotNone(self.calc.subtracao(20,30))
+    
+    def test_potenciacao(self):
+        self.assertEqual(self.calc.potenciacao(5,2),25)
+        self.assertEqual(self.calc.potenciacao(10,3),1000)
+
 if __name__ == '__main__':
     unittest.main()
